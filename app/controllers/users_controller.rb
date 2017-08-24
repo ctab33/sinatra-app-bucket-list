70eed9046d4107970ccc583ae64af4d423d5dc33
ctab33 <ctab33@me.com>
2017-08-24 19:59:56 -0400

    new user form created, post signup in controller tentatively done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/new'
+    else
+      redirect to 'goals'
+    end
+  end
+
   post '/login' do
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
@@ -16,6 +24,12 @@ class UsersController < ApplicationController
     else
       redirect to 'signup'
     end
+  end
+
+  post '/signup' do #check validation before submitting 
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+    redirect to '/lists'
   end
 
 end
