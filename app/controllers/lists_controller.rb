8ce6a02a06dac6322ae17d51fd7e1ac949a1a17f
ctab33 <ctab33@me.com>
2017-08-29 18:06:43 -0400

    patch, delete and edit done for lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,53 +1,75 @@
 class ListsController < ApplicationController
-
-
   get '/lists' do
+   if logged_in?
+     @lists = current_user.lists
+     erb :'lists/index'
+   else
+     redirect to '/login'
+   end
+ end
+
+ get '/lists/new' do
+   if logged_in?
+     erb :'lists/new'
+   else
+     redirect to '/login'
+   end
+ end
+
+ post '/lists' do
+   if params[:name] == ""
+     redirect to '/lists/new'
+   else
+     #current_user.lists.create(params)
+     @list = List.new(params)
+     @list.user_id = session[:user_id]
+     @list.save
+     redirect to "/lists"
+   end
+ end
+
+ get '/lists/:id' do
+   if logged_in?
+    @list = List.find_by_id(params[:id])
+    erb :'lists/show'
+  else
+    redirect to "/login"
+  end
+ end
+
+ ##### EDIT LIST ACTIONS #####
+
+ get '/lists/:id/edit' do #load edit form
     if logged_in?
-      @lists = current_user.lists
-      erb :'lists/index'
+      @list = List.find_by_id(params[:id])
+      if @list.user_id == current_user.id
+        erb :'/lists/edit'
+      else
+        redirect to '/lists'
+      end
     else
       redirect to '/login'
     end
-  end
-
-  get '/lists/new' do
-    if logged_in?
-      erb :'lists/new'
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/lists/:id/edit' do #load edit form
-    @list = List.find_by_id(params[:id])
-    erb :'/lists/edit'
-  end
-
-  post '/lists' do
-    if params[:name] == ""
-      redirect to '/lists/new'
-    else
-      current_user.lists.create(params)
-      redirect to "/lists"
-    end
-  end
-
-  get '/lists/:id' do
-    @list = List.find_by_id(params[:id])
-    erb :'lists/edit'
-  end
-
-  delete '/lists/:id/delete' do
-    @list = List.find_by_id(params[:id])
-    @list.destroy
-    redirect to '/lists'
   end
 
   patch '/lists/:id' do #edit action
     @list = List.find_by_id(params[:id])
-    @list.name = params[:title]
-    @list.save
+    @list.update(name: params[:name])
     redirect to "/lists/#{@list.id}"
   end
 
-end
+  delete '/lists/:id/delete' do
+    if logged_in?
+      @list = List.find(params[:id])
+      if @list.user_id == current_user.id
+        @list.delete
+        redirect to '/lists'
+      else
+        redirect to '/lists'
+      end
+    else
+      redirect to '/login'
+    end
+ end
+
+end #end controller
