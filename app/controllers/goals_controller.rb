69fd8960b2bc9b06982616baf94120a96c2c7402
ctab33 <ctab33@me.com>
2017-08-30 13:56:51 -0400

    edit get and post for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,4 +8,19 @@ class GoalsController < ApplicationController
     @lists = current_user.lists
     erb :'goals/new'
   end
+
+  get '/goals/:id/edit' do
+    @goal = Goal.find(params[:id])
+    @lists = current_user.lists
+    erb :'goals/edit'
+  end
+
+  patch '/goals/:id/' do
+    @goal = Goal.find(params[:id])
+    @goal.update(name: params[:name])
+    redirect to "/goals/#{@goal.id}"
+  end
+
+  
+
 end
